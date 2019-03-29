5aa9ee0cd9e15416840eed2a63b22e7bce2e4054
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-03-28 21:22:42 -0500

    required express & created database & table

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id int NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(75) NOT NULL,
+devoured tinyint(0) DEFAULT NULL,
+PRIMARY KEY (id)
+);
+
+;show table 
+SELECT * FROM burgers;
+
+-- NOTES --
+-- "MySQL doesn't support true booleans as per standard SQL. --
+-- It uses integer values 1 and 0 respectively for true and false. --
+-- It does support the keywords true and false, but these are mapped to the integer values." --
+
+-- Documentation: BOOL, BOOLEAN --
+-- These types are synonyms for TINYINT(1). A value of zero is considered false. Nonzero values are considered true --
--- server.js
+++ server.js
@@ -1,3 +1,30 @@
 // ============ DEPENDENCIES =========
 // Require express
 var express = require('express');
+
+/* ================= NOTES ============================
+    In order to implement Express as the HTTP server for this node.js app,
+    we must create an instance of Express and begin listening on a port (add app.listen(port) at the end of this file)
+*/
+
+// Set up the Express App & create the required instance of Express to act as the HTTP server for this application.
+var app = express();
+// Define localhost PORT we want to listen to
+var PORT = process.env.PORT || 3000;
+
+//Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("App listening on PORT: " + PORT);  //app listening ✓
+});
