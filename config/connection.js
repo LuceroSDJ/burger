16877781a2a2d74a7ee04545d5dd4766c8e043b3
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-04-01 14:32:08 -0500

    set up JawsDB provision

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // require mysql & setup the code to connect Node to MySQL.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Exito2019*",
-  database: "burgers_db"
-});
+/* 5Part	Two:	Hooking	Your	Project	withJawsDB	10.
+In	your	connection.js(or	whichever	fileyoucreatedyour	MySQL	connection),	
+add	the	code	shownin	the	image	below	(additionshighlighted):  */
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Exito2019*",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
